b9499771f0e6c1f3c3a79181d2d387cba928fe9d
John Arild Lolland <lollandster@gmail.com>
2016-11-03 20:27:29 +0100

    Now works as the VS481B for the commands I care about

--- Switch-simulator.py
+++ Switch-simulator.py
@@ -4,9 +4,31 @@
 
 import serial
 
-ser = serial.Serial('/dev/ttyS0')
+ser = serial.Serial('/dev/ttyUSB0')
 ser.baudrate = 19200
 ser.close()
 ser.open()
-ser.write("read Command OK\n\r Input:port 2\n\r Output:ON\n\r Mode:Next\n\r Goto:OFF\n\r F/W:V1.0.062\n\r")
-ser.close()
+#ser.write("read Command OK\n\r Input:port 2\n\r Output:ON\n\r Mode:Next\n\r Goto:OFF\n\r F/W:V1.0.062\n\r")
+#ser.close()
+inputport = "Input:port 2"
+Output = "Output:ON"
+
+while True:
+    x = ser.readline()
+    x = ":".join("{:02x}".format(ord(c)) for c in x)
+    print(x)
+    if x == "72:65:61:64:0d:0a":
+        print("ok")
+        ser.write("read Command OK\n\r " + inputport + "\n\r " + Output + "\n\r Mode:Next\n\r Goto:OFF\n\r F/W:V1.0.062\n\r")
+
+    if x == "73:77:20:6f:6e:0d:0a":
+        Output = "Output:ON"
+
+    if x == "73:77:20:6f:66:66:0d:0a":
+        Output = "Output:OFF"
+
+    if x == "73:77:20:69:30:31:0d:0a":
+        inputport = "Input:port 1"
+
+    if x == "73:77:20:69:30:32:0d:0a":
+        inputport = "Input:port 2"
